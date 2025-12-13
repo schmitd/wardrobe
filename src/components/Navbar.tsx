9714d14e1f97b05c0d0f96c78812283703d72da1
David Schmitt <davidschmittgit@gmail.com>
2025-12-13 00:40:06 -0500

    Add useSubscription hook to Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Shirt } from 'lucide-react';
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
-import { useSubscription } from '@/hooks/useSubscription';
+import { useSubscription } from '../hooks/useSubscription';
 
 
 
