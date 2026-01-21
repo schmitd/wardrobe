6302e048e20ad2f9f5b1bb089794037a33a6f4fa
David Schmitt <davidschmittgit@gmail.com>
2026-01-21 13:41:32 -0500

    chore: enable baseUrl in qstash client for flexibility

--- src/lib/qstash.ts
+++ src/lib/qstash.ts
@@ -1,7 +1,10 @@
 import { Client } from "@upstash/qstash";
 
 export const qstashClient = process.env.QSTASH_TOKEN
-    ? new Client({ token: process.env.QSTASH_TOKEN })
+    ? new Client({
+        token: process.env.QSTASH_TOKEN,
+        baseUrl: process.env.QSTASH_URL
+    })
     : null;
 
 export const APP_URL = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
