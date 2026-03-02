900e8ec18628c43f3019d24bbe3a56069be87d7f
David Schmitt <davidschmittgit@gmail.com>
2026-03-01 20:09:25 -0500

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -43,6 +43,7 @@ function Button({
   variant = "default",
   size = "default",
   asChild = false,
+  type,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
@@ -55,9 +56,12 @@ function Button({
       data-slot="button"
       data-variant={variant}
       data-size={size}
+      type={asChild ? undefined : (type ?? "button")}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
+  )
+}
   )
 }
 
